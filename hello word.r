d381da084a072afe44fff96f43eecaa4815c1056
Rachapak3 <143682609+Rachapak3@users.noreply.github.com>
2023-08-31 14:24:01 +0700

    Delete hello word.r

--- hello word.r
+++
@@ -1,9 +0,0 @@
-## การสร้าง dataframe ใน ภาษา R
-สร้างข้อมูล
-firends <- c("pak", "bell", "cat", "dog","ant")
-age <- c(34,35,29,30,44)
-movie_lover <- c(TRUE,TRUE,FALSE,TRUE,FALSE)
-	ใช้ function
-df <- data.frame(firends,age,movie_lover)
-แสดง ในรูปแบบต่างราง
-df
